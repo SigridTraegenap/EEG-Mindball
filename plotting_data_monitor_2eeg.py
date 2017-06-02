73ee4c3c160b51a1199e571834bc2533e3aa4c09
EEG <traegenap@fias.uni-frankfurt.de>
2017-06-02 18:26:55 +0200

    colors

--- plotting_data_monitor_2eeg.py
+++ plotting_data_monitor_2eeg.py
@@ -19,6 +19,14 @@ from libs.utils import get_all_from_queue, get_item_from_queue
 from libs.decode import decode_output
 from livedatafeed import LiveDataFeed
 
+color1 = "limegreen"
+color2 = "magenta"
+width_signal = 5
+
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
+#fixes to white background and black labels
+
 
 class PlottingDataMonitor(QMainWindow):
 	def __init__(self, parent=None):
@@ -40,11 +48,12 @@ class PlottingDataMonitor(QMainWindow):
 		self.x_low = 0.1
 		self.x_high = 3
 		
-		self.ball_coordx = [0.]
-		self.ball_coordy = [0.]
-		self.tuning_factor = 1.
+		self.ball_coordx = 0.
+		self.ball_coordy = 0.
+		self.tuning_factor = 0.1
 		self.text_html = '<div style="text-align: center"><span style="color: #FFF; font-size: 40pt">Goal</span><br><span style="color: #FFF; font-size: 40pt; text-align: center"> {} is winner </span></div>'
 		self.show_one_item = False
+		self.winner_text = None
 	
 	def create_plot(self, xlabel, ylabel, xlim, ylim, curve_style=None, ncurves=1):
 		plot = pg.PlotWidget()
@@ -62,18 +71,42 @@ class PlottingDataMonitor(QMainWindow):
 		#plot.setCanvasBackground(Qt.black)
 		plot.replot()
 		
-		pen = QPen(QColor('limegreen'))
-		#pen.setWidth(0.9)
+		pen = QPen(QColor(color1))
+		#pen.setWidth(1.5)
+		#setting this width increases also fft width - do not use 
 		curve.setPen(pen)
 		
 		if ncurves==2:
 			curve2 = plot.plot(symbol=curve_style)
 			#pen.setWidth(0.9)
-			pen2 = QPen(QColor('magenta'))
+			pen2 = QPen(QColor(color2))
 			curve2.setPen(pen2)
 			return plot, curve, curve2
 		else:
 			return plot, curve
+
+	def create_arenaplot(self, xlabel, ylabel="Player "+color1, xlim=[-1,1], ylim=[-1,1], curve_style=None):
+		plot = pg.PlotWidget(background=QColor("#217300"))
+		if curve_style is not None:
+			curve = plot.plot(symbol=curve_style,antialias=True, symbolSize=15)
+			#brush = QBrush(QColor('limegreen'))
+			#curve.setBrush(brush)
+		else:
+			curve = plot.plot(antialias=True)
+		plot.setLabel('left', ylabel)
+		plot.setLabel('bottom', xlabel)
+		plot.setXRange(xlim[0], xlim[1])
+		plot.setYRange(ylim[0], ylim[1])
+
+		#plot.setCanvasBackground(Qt.black)
+		plot.replot()
+		
+		pen = QPen(QColor(color1))
+		#pen.setWidth(1.5)
+		#setting this width increases also fft width - do not use 
+		curve.setPen(pen)
+
+		return plot, curve
 
 	def create_status_bar(self):
 		self.status_text = QLabel('Monitor idle')
@@ -87,7 +120,7 @@ class PlottingDataMonitor(QMainWindow):
 		#main_layout.setSpacing(3)
 		#main_layout.setRowStretch(1, 2)
 		#main_layout.setColumnStretch(1, 1)
-		main_layout.setColumnStretch(0, 1)
+		main_layout.setColumnStretch(0,1)
 
 
 		## Plot
@@ -104,7 +137,7 @@ class PlottingDataMonitor(QMainWindow):
 		
 		### Arena
 		###
-		self.plot_arena, self.curve_arena = self.create_plot('x', 'y', [-1,1,0.2], [-1,1,0.2], curve_style='o')
+		self.plot_arena, self.curve_arena = self.create_arenaplot(' ', 'Y', [-1,1,0.2], [-1,1,0.2], curve_style='o')
 		
 		plot_layout_arena = QHBoxLayout()
 		plot_layout_arena.addWidget(self.plot_arena)
@@ -121,6 +154,13 @@ class PlottingDataMonitor(QMainWindow):
 		self.setGeometry(30, 30, 950, 300)
 		
 		self.setCentralWidget(self.main_frame)
+
+###rewrite (trial) Sigrid to implement two windows
+		#self.main_frame 
+		
+
+
+
 
 	def create_menu(self):
 		self.file_menu = self.menuBar().addMenu("&File")
@@ -164,12 +204,26 @@ class PlottingDataMonitor(QMainWindow):
 		if self.com_monitor is not None:
 			self.com_monitor.join(0.01)
 			self.com_monitor = None
-
+		
+		if self.com_monitor2 is not None:
+			self.com_monitor2.join(0.01)
+			self.com_monitor2 = None
+		
 		self.monitor_active = False
 		self.timer.stop()
+		self.timer_plot.stop()
 		self.set_actions_enable_state()
 		
 		self.status_text.setText('Monitor idle')
+	
+	def reset_arena(self):
+		"""bring ball back to center, remove winner sign"""
+		#self.plot_arena.clear()
+		self.plot_arena.removeItem(self.winner_text)
+		self.show_one_item = False
+		
+		self.ball_coordx, self.ball_coordy = 0,0
+		self.curve_arena.setData([self.ball_coordx], [self.ball_coordy])
 	
 	def on_start(self):
 		""" Start the monitor: com_monitor thread and the update
@@ -177,6 +231,9 @@ class PlottingDataMonitor(QMainWindow):
 		"""
 		if self.com_monitor is not None:
 			return
+			
+		if self.show_one_item is True:
+			self.reset_arena()
 		
 		self.data_q = Queue.Queue()
 		self.error_q = Queue.Queue()
@@ -198,6 +255,7 @@ class PlottingDataMonitor(QMainWindow):
 		
 		com_error = get_item_from_queue(self.error_q)
 		com_error2 = get_item_from_queue(self.error2_q)
+
 		if com_error is not None:
 			QMessageBox.critical(self, 'ComMonitorThread error',
 				com_error)
@@ -206,7 +264,7 @@ class PlottingDataMonitor(QMainWindow):
 			QMessageBox.critical(self, 'ComMonitorThread error',
 				com_error2)
 			self.com_monitor2 = None
-			
+		
 		self.monitor_active = True
 		self.set_actions_enable_state()
 		
@@ -217,7 +275,7 @@ class PlottingDataMonitor(QMainWindow):
 		
 		self.timer_plot = QTimer()
 		self.connect(self.timer_plot, SIGNAL('timeout()'), self.on_timer_plot)
-		update_freq = 100. #Hz
+		update_freq = 10. #Hz
 		self.timer_plot.start(1000.0 / update_freq) #ms
 		
 		self.status_text.setText('Monitor running')
@@ -264,6 +322,7 @@ class PlottingDataMonitor(QMainWindow):
 			#
 			delta = np.array(xdata[1:])-np.array(xdata[:-1])
 			#print(np.nanmean(delta),xdata[-1])#,np.nanstd(delta)
+			#print(xdata[-1])
 			n = len(ydata)
 			fft1 = np.abs(np.fft.rfft(ydata))
 			fft1 = (fft1/np.sum(fft1))[1:]
@@ -298,18 +357,23 @@ class PlottingDataMonitor(QMainWindow):
 		if (update1 and update2):
 			if n>999:
 				#print((power_alpha2 - power_alpha)*self.tuning_factor)
-				self.ball_coordx[0] += (power_alpha2 - power_alpha)*self.tuning_factor
-				self.ball_coordy[0] += 0
-			else:
-				self.ball_coordx[0] = 0
-				self.ball_coordy[0] = 0
-			self.curve_arena.setData(max(1,self.ball_coordx),self.ball_coordy, _CallSync='off')
-			if abs(self.ball_coordx[0])>0.1 and self.show_one_item is False:
-				winner_color = 'limegreen' if self.ball_coordx[0]<0 else 'Magenta'
-				text = pg.TextItem(html=self.text_html.format(winner_color), anchor=(0.3,1.3),\
+				self.ball_coordx += (power_alpha2 - power_alpha)*self.tuning_factor
+				self.ball_coordy += np.random.normal(scale=0.05)
+				#self.ball_coordy[0] += 0
+			#else:
+				#self.ball_coordx[0] = 0
+				#self.ball_coordy[0] = 0
+			print(self.ball_coordx, self.ball_coordy)
+			self.curve_arena.setData([np.sign(self.ball_coordx)*min(1, abs(self.ball_coordx))], [self.ball_coordy], _CallSync='off')
+
+			if abs(self.ball_coordy)>0.7:
+				self.ball_coordy = self.ball_coordy - 0.3*self.ball_coordy
+			if abs(self.ball_coordx)>1 and self.show_one_item is False:
+				winner_color = color1 if self.ball_coordx<0 else color2
+				self.winner_text = pg.TextItem(html=self.text_html.format(winner_color), anchor=(0.3,1.3),\
 				border=QColor(winner_color), fill=(201, 165, 255, 100))
 				
-				self.plot_arena.addItem(text)
+				self.plot_arena.addItem(self.winner_text)
 				self.show_one_item = True
 				self.on_stop()
 	
