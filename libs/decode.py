fa43a3ddda6eb39800a755933e2a9fac0c9b3698
btna <bettina@debian>
2017-06-04 16:51:24 +0200

    allow also decoding when main frame is odd line number

--- libs/decode.py
+++ libs/decode.py
@@ -3,8 +3,12 @@ import numpy as np
 def decode_output(line):
 	line = np.fromstring(line,dtype=np.uint8)
 	foundBeginingOfFrame = 0
+	delta = 1
 	result = []
-	for i in np.arange(0,len(line)-1,2):
+	i = 0
+	#print(len(line))
+	while i<(len(line)-1):
+		#print(i)
 		if foundBeginingOfFrame==0:
 			if(line[i]>127):
 				#print('foundBeginingOfFrame')
@@ -13,9 +17,15 @@ def decode_output(line):
 				intout = np.uint16(np.uint16(line[i] & 127)*128)
 				intout = intout + np.uint16(line[i+1])
 				result.append(intout)
+				i += 2
+			else:
+				i += 1
 		else:
 			##extract one sample from 2 bytes
 			intout = np.uint16(np.uint16(line[i] & 127)*128);
 			intout = intout + np.uint16(line[i+1]);
 			result.append(intout)
+			i += 2
 	return result
+			
+	
